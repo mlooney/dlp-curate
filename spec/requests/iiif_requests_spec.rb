672ad42ca2a35ed3a2e1bd29529f89b202106f59
Rachel Kadel <rachelkg@gmail.com>
2020-03-05 14:37:31 -0500

    Adds a request spec for the iiif controller

---
+++ spec/requests/iiif_requests_spec.rb
@@ -0,0 +1,62 @@
+
+# frozen_string_literal: true
+require "rails_helper"
+include Warden::Test::Helpers
+
+RSpec.describe "IIIF requests", :clean, type: :request do
+  let(:work_id) { "436tx95xcc-cor" }
+  let(:image_sha) { "79276774f3dbfbd977d39065eec14aa185b5213d" }
+  let(:region) { "full" }
+  let(:size) { "full" }
+  let(:rotation) { 0 }
+  let(:quality) { "default" }
+  let(:format) { "jpg" }
+  let(:params) do
+    {
+      identifier: image_sha,
+      region:     region,
+      size:       size,
+      rotation:   rotation,
+      quality:    quality,
+      format:     format
+    }
+  end
+
+  before do
+    ENV['IIIF_MANIFEST_CACHE'] = Rails.root.join('tmp').to_s
+    ENV['PROXIED_IIIF_SERVER_URL'] = 'https://iiif-cor-arch.library.emory.edu/cantaloupe/iiif/2'
+    WebMock.allow_net_connect!
+  end
+  describe "GET image" do
+    context "with a public object" do
+      let(:visibility_ssi) {"open"}
+      it "responds with a success status" do
+        get "/iiif/2/#{image_sha}/#{region}/#{size}/#{rotation}/#{quality}.#{format}"
+        expect(response.status).to eq 200
+        expect(response.has_header?("Access-Control-Allow-Origin")).to be_truthy
+        expect(response.get_header("Access-Control-Allow-Origin")).to eq "*"
+      end
+    end
+    context "with an Emory High Download object" do
+      before do
+        get "/iiif/2/#{image_sha}/#{region}/#{size}/#{rotation}/#{quality}.#{format}"
+      end
+      let(:visibility_ssi) {"authenticated"}
+      it "responds with a success status" do
+        expect(response.status).to eq 200
+      end
+      xit "has access control header limited to curate" do
+        expect(response.has_header?("Access-Control-Allow-Origin")).to be_truthy
+        byebug
+        expect(response.get_header("Access-Control-Allow-Origin")).to eq "https://digital-arch.library.emory.edu"
+      end
+    end
+  end
+  # describe "GET manifest" do
+  #   context "with a public object" do
+  #     it "responds with a success status" do
+  #       get "/iiif/"
+  #     end
+  #   end
+  # end
+end
